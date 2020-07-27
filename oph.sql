5e8fa466f19fd03f26949e66b5bbec5c446d8818
DDKK64 <52639829+DDKK64@users.noreply.github.com>
2020-07-27 12:56:57 +0800

    Create oph.sql
    init

---
+++ oph.sql
@@ -0,0 +1,33 @@
+CREATE TABLE oph_elder (
+  id VARCHAR(32),
+  name VARCHAR(32),
+  sex VARCHAR(16),
+  birthday DATE,
+  joinday DATE,
+  leaveday DATE,
+  int roomid,
+  PRIMARY KEY (id),
+  FOREIGN KEY (roomid) REFERENCE oph_room(room_id)
+);
+
+CREATE TABLE oph_employee (
+  id VARCHAR(32),
+  name VARCHAR(32),
+  sex VARCHAR,
+  birthday Date,
+  joinday DATE,
+  leaveday DATE,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE oph_room (
+  id VARCHAR(16),
+  type int,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE oph_department (
+  id VARCHAR(16),
+  name VARCHAR(64),
+);
+--part I end--
